db279b9f87d1f624f268ad0792cdfaf2f9e29c18
Priscilla-D <durand.priscilla@outlook.fr>
2023-12-29 13:59:05 +0100

    improve types

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,8 +2,8 @@ import { AnswerObject } from "../App";
 import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 
 type Props = {
-  question?: string;
-  answers?: any[];
+  question: string;
+  answers: string[];
   callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
   userAnswer: AnswerObject | undefined;
   questionNr: number;
